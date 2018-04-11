27f3ed10cbfffce8a3960d0c4842a64aca53bce7
Levrero <32905591+Levrero@users.noreply.github.com>
2018-04-11 17:15:52 +0100

    Update input.f90

--- sources/input.f90
+++ sources/input.f90
@@ -108,13 +108,9 @@ MODULE INPUT
     !    vectors with the data
     !----------------------------------------------------------------------
 
-      IF (numpe==1) THEN
+      IF (numpe == 1) THEN
         OPEN(10,FILE=fname,STATUS='OLD',ACTION='READ')
-        READ(10,*)nels,nn,nr,loaded_nodes,fixed_nodes,nip
-        READ(10,*)limit,tol,e,v
-        READ(10,*)nod
-        READ(10,*)num_load_steps,jump
-        READ(10,*)tol2
+        READ(10,*) nels, nn, nr, loaded_nodes, fixed_nodes, nod, nip
         CLOSE(10)
       
         vec_integer(1) = nels
@@ -155,19 +151,17 @@ MODULE INPUT
 !------------------------------------------------------------------------------
 !------------------------------------------------------------------------------
 
-    SUBROUTINE READ_DATA_XX7(fname,numpe,nels,nn,nr,loaded_nodes,fixed_nodes, &
-                             nip,limit,tol,e,v,nod,num_load_steps,jump,tol2)
+SUBROUTINE READ_NUMERICAL_DATA(fname,numpe,nels,nn,nr,loaded_nodes,fixed_nodes,&
+     nod,nip)
 
-    !/****f* input_output/read_data_xx7
+    !/****f* input_output/read_numerical_data
     !*  NAME
-    !*    SUBROUTINE: read_data_xx7
+    !*    SUBROUTINE: read_numerical_data
     !*  SYNOPSIS
-    !*    Usage:      CALL read_data_xx7(fname,numpe,nels,nn,nr,              &
-    !*                                   loaded_nodes,fixed_nodes,nip,        &
-    !*                                   limit,tol,e,v,nod,num_load_steps,    &
-    !*                                   jump,tol2)
+    !*    Usage:      CALL read_numerical_data(fname,numpe,nels,nn,nr,loaded_nodes,&
+    !*                                fixed_nodes,nod,nip)
     !*  FUNCTION
-    !*    Master process reads the general data of the problem
+    !*    Master process reads the numerical data of the problem
     !*    Master process broadcasts to slave processes.
     !*  INPUTS
     !*    The following arguments have the INTENT(IN) attribute:
@@ -178,55 +172,26 @@ MODULE INPUT
     !*    numpe                  : Integer
     !*                           : Process number
     !*
-    !*    The following arguments have the INTENT(OUT) attribute:
-    !*
-    !*    nels                   : Integer
-    !*                           : Total number of elements
-    !*
-    !*    nn                     : Integer
-    !*                           : Total number of nodes 
-    !*
-    !*    nr                     : Integer
-    !*                           : Number of nodes with restrained degrees of
-    !*                             freedom 
-    !*
-    !*    loaded_nodes           : Integer
-    !*                           : Number of nodes with applied forces
-    !*
-    !*    fixed_nodes            : Integer
-    !*                           : Number of restrained degrees of freedom 
-    !*                             with a non-zero applied value
-    !*
-    !*    nip                    : Integer
-    !*                           : Number of Gauss integration points
-    !*
     !*    limit                  : Integer
     !*                           : Maximum number of PCG iterations allowed
     !*
     !*    tol                    : Real
-    !*                           : Tolerance for PCG
     !*
-    !*    e                      : Real
-    !*                           : Young's modulus
+    !*    time                   : Real
+    !*                           : Time interval of the simulation
     !*
-    !*    v                      : Real
-    !*                           : Poisson coefficient
-    !*
-    !*    nod                    : Integer
-    !*                           : Number of nodes per element
-    !*
-    !*    num_load_steps         : Integer
-    !*                           : Number of load steps
+    !*    max_steps              : Integer
+    !*                             Maximum number of load steps
     !*
     !*    jump                   : Integer
     !*                           : Number of load steps to skip before writing
     !*                             results (periodically)
     !*
-    !*    tol2                   : Real
+    !*    nr_tol                 : Real
     !*                           : Tolerance for Newton-Raphson loop
     !*
     !*  AUTHOR
-    !*    Francisco Calvo
+    !*    Francesc Levrero-Florencio
     !*    L. Margetts
     !*  CREATION DATE
     !*    01.06.2007
@@ -237,81 +202,57 @@ MODULE INPUT
     !*
     !*/
   
-    IMPLICIT NONE
+      IMPLICIT NONE
 
-    CHARACTER(*), INTENT(IN)  :: fname
-    INTEGER,      INTENT(IN)  :: numpe
-    INTEGER,      INTENT(OUT) :: nels, nn, nr, loaded_nodes, fixed_nodes, nip,&
-                                 limit, nod, num_load_steps, jump
-    REAL(iwp),    INTENT(OUT) :: tol, e, v, tol2
-    INTEGER                   :: bufsize, ier, vec_integer(10)
-    REAL(iwp)                 :: vec_real(4)
+      CHARACTER(*), INTENT(IN)  :: fname
+      INTEGER,      INTENT(IN)  :: numpe
+      INTEGER,      INTENT(OUT) :: limit, max_steps, jump
+      REAL(iwp),    INTENT(OUT) :: tol, nr_tol
+      INTEGER                   :: bufsize, ier, vec_integer(3)
+      REAL(iwp)                 :: vec_real(2)
 
     !----------------------------------------------------------------------
     ! 1. Master process reads the data and builds the integer and real
     !    vectors with the data
     !----------------------------------------------------------------------
 
-    IF (numpe==1) THEN
-      OPEN(10,FILE=fname,STATUS='OLD',ACTION='READ')
-      READ(10,*)nels,nn,nr,loaded_nodes,fixed_nodes,nip
-      READ(10,*)limit,tol,e,v
-      READ(10,*)nod
-      READ(10,*)num_load_steps,jump
-      READ(10,*)tol2
-      CLOSE(10)
+      IF (numpe == 1) THEN
+        OPEN(10,FILE=fname,STATUS='OLD',ACTION='READ')
+        READ(10,*) limit, tol, max_steps, jump, nr_tol
+        CLOSE(10)
       
-      vec_integer(1)  = nels
-      vec_integer(2)  = nn
-      vec_integer(3)  = nr
-      vec_integer(4)  = loaded_nodes
-      vec_integer(5)  = fixed_nodes
-      vec_integer(6)  = nip
-      vec_integer(7)  = limit
-      vec_real(1)     = tol
-      vec_real(2)     = e
-      vec_real(3)     = v
-      vec_integer(8)  = nod
-      vec_integer(9)  = num_load_steps
-      vec_integer(10) = jump
-      vec_real(4)     = tol2
-      
-    END IF
+        vec_integer(1) = limit
+        vec_real(1)    = tol
+        vec_integer(2) = max_steps
+        vec_integer(3) = jump
+        vec_real(2)    = nr_tol
+      END IF
 
     !----------------------------------------------------------------------
     ! 2. Master process broadcasts the data to slave processes
     !----------------------------------------------------------------------
 
-    bufsize = 10
-    CALL MPI_BCAST(vec_integer,bufsize,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
+      bufsize = 2
+      CALL MPI_BCAST(vec_integer,bufsize,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
 
-    bufsize = 4
-    CALL MPI_BCAST(vec_real,bufsize,MPI_REAL8,0,MPI_COMM_WORLD,ier)
+      bufsize = 2
+      CALL MPI_BCAST(vec_real,bufsize,MPI_REAL8,0,MPI_COMM_WORLD,ier)
 
     !----------------------------------------------------------------------
     ! 3. Slave processes extract the variables back from the vectors
     !----------------------------------------------------------------------
 
-    IF (numpe/=1) THEN
-      nels           = vec_integer(1)
-      nn             = vec_integer(2)
-      nr             = vec_integer(3)
-      loaded_nodes   = vec_integer(4)
-      fixed_nodes    = vec_integer(5)
-      nip            = vec_integer(6)
-      limit          = vec_integer(7)
-      tol            = vec_real(1)
-      e              = vec_real(2)
-      v              = vec_real(3)
-      nod            = vec_integer(8)
-      num_load_steps = vec_integer(9)
-      jump           = vec_integer(10)
-      tol2           = vec_real(4)
-    END IF
+      IF (numpe /= 1) THEN
+        limit     = vec_integer(1)
+        tol       = vec_real(1)
+        max_steps = vec_integer(2)
+        jump      = vec_integer(3)
+        nr_tol    = vec_real(2)
+      END IF
 
-    RETURN
+      RETURN
 
-  END SUBROUTINE READ_DATA_XX7
+    END SUBROUTINE READ_NUMERICAL_DATA
 
 !------------------------------------------------------------------------------
 !------------------------------------------------------------------------------
